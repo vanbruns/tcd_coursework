d24c8805927a97bb73696e70a467ce4e3cea5e6c
Van Bruns Jr <van.bruns@gmail.com>
2025-05-09 15:44:12 -0700

    Delete CS7GV3/Assignment 1 directory

--- CS7GV3/Assignment 1/Lab/Shaders/simpleFragmentShader.glsl
+++
@@ -1,9 +0,0 @@
-#version 330
-
-in vec3 normal_eye;
-
-out vec4 fragment_colour;
-
-void main(){
-	fragment_colour = vec4 (normal_eye, 1.0);
-}
--- CS7GV3/Assignment 1/README.md
+++
@@ -1,2 +0,0 @@
-# About
-Code for Assignment 1 of Real-Time Rendering at Trinity College Dublin Spring 2020.
